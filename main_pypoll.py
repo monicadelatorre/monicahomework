c72344853ccf15a5ee7629886544adba3fb74be0
GitMD <mgdelatorre@gmail.com>
2018-12-08 09:29:44 -0800

    pypoll_push2

--- main_pypoll.py
+++ main_pypoll.py
@@ -3,12 +3,14 @@ import csv
 import operator
 
 # Find the csv file
-csvpath = os.path.join("..", "Resources", "election_data_med.csv")
-budget_data_out = open("election_data_out.txt",'w')
-budget_data_out.close()
-csvout = os.path.join("..", "Resources", "election_data_out.txt")
+csvpath = os.path.join("..", "Resources", "election_data_short.csv")
 
+#create output file
+dataout = open("election_results.txt",'w')
+dataout.close()
+csvout = os.path.join("..", "Resources", "election_results.txt")
 
+#lists
 voterid = []
 county = []
 candidate = []
@@ -31,26 +33,28 @@ with open(csvpath) as csvfile:
         
         # Grab candidate and store it into a list
         candidate = candidate + [row[2]]
-        totalvotes = len(candidate)
         
-        #count how many votes each candidate received
-        countkahn = candidate. count('Khan')
-        countCorrey = candidate. count('Correy')
-        countLi = candidate. count('Li')
-        countTooley = candidate. count("O'Tooley")
+    #calculations using python list called candidate
+    totalvotes = len(candidate)
         
-        #calculate percentage
-        pk = (countkahn/totalvotes)*100
-        formatpk = "{0:.3f}".format(pk)
+    #count how many votes each candidate received
+    countkahn = candidate. count('Khan')
+    countCorrey = candidate. count('Correy')
+    countLi = candidate. count('Li')
+    countTooley = candidate. count("O'Tooley")
         
-        pc = (countCorrey/totalvotes)*100
-        formatpc = "{0:.3f}".format(pc)
+    #calculate percentage
+    pk = (countkahn/totalvotes)*100
+    formatpk = "{0:.3f}".format(pk)
         
-        pl = (countLi/totalvotes)*100
-        formatpl = "{0:.3f}".format(pl)
+    pc = (countCorrey/totalvotes)*100
+    formatpc = "{0:.3f}".format(pc)
         
-        pt = (countTooley/totalvotes)*100
-        formatpt = "{0:.3f}".format(pt)
+    pl = (countLi/totalvotes)*100
+    formatpl = "{0:.3f}".format(pl)
+        
+    pt = (countTooley/totalvotes)*100
+    formatpt = "{0:.3f}".format(pt)
         
 #    print(countkahn)
 #    print(totalvotes)
@@ -63,11 +67,15 @@ with open(csvpath) as csvfile:
     print ("Correy: " + str(formatpc)+"%"+" ("+ str(countCorrey)+")")
     print ("Li: " + str(formatpl)+"%"+" ("+ str(countLi)+")")
     print ("O'Tooley: " + str(formatpt)+"%"+" ("+ str(countTooley)+")")
-        
+       
+    #create a dictionary with results from calculations
     cand_dict = {"Kahn":countkahn,"Correy":countCorrey,"Li":countLi,"O'Tooley":countTooley}
+    
+    #find the max value in the dictionary called cand_dict to find the winner
     b = max(cand_dict.items(), key=operator.itemgetter(1))[0]
     print ("Winner: " + str(b))
     
+    #print to the txt file that was created 
     with open(csvout,"w") as candout:  
         candout.write("Election Results"+"\n")
         candout.write("--------------------"+"\n")
